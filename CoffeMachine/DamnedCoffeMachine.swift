7cdf9df98d7e809129c558cbe0f7f435495b8e07
Ivan <Ivan@macbook-pro-umanets.local>
2019-02-24 16:10:04 +0200

    connect Label

--- CoffeMachine/DamnedCoffeMachine.swift
+++ CoffeMachine/DamnedCoffeMachine.swift
@@ -9,41 +9,108 @@
 import UIKit
 
 class DamnedCoffeMachine: NSObject {
+    
+    var expiredMilk = 0
+    var holyWater = 0
+    var evilSeeds = 0
+    var cocaine = 0
+    
+    
+    func addHolyWater() {
+        holyWater += 666
+    }
+    
+    func addEvilSeeds() {
+        evilSeeds += 666
+    }
+    
+    func addExpiredMilk() {
+        expiredMilk += 666
+    }
+    
+    func addCocaine() {
+        cocaine += 666
+    }
 
-    var expiredMilk = 666
-    var holyWater = 666
-    var evilSeeds = 666
+    
+//    init(expiredMilk: Int, holyWater: Int, evilSeeds: Int, cocaine: Int) {
+//        self.expiredMilk = expiredMilk
+//        self.holyWater = holyWater
+//        self.evilSeeds = evilSeeds
+//    }
     
     
-    
-    var coffee : Int{
-    return holyWater + evilSeeds
-    
-    
-       
-    
-    
+    func makeShitCoffee(name: String, seedsNeed: Int, holyWaterNeed: Int, expiredMilkNeed: Int, cocaineNeed: Int) {
+        if evilSeeds > seedsNeed {
+            if holyWater > holyWaterNeed {
+                if expiredMilk > expiredMilkNeed {
+                    if cocaine > cocaineNeed {
+                        evilSeeds -= seedsNeed
+                        holyWater -= holyWaterNeed
+                        expiredMilk -= expiredMilkNeed
+                        cocaine -= cocaineNeed
+                        print ("Your shit \(name) is ready, bitch!")
+                    } else {
+                        print("Need some Florida snow, dude!")
+                    }
+                } else {
+                    print ("If you want to save your soul, add milk!")
+                }
+            }
+            else {
+                print ("Hey, moron, you`d better add water or I`ll kick yo ass!")
+            }
+        }
+        else {
+            print ("Box for seeds is empty, fool!")
+        }
     }
 
     func makingHotLikeHellAmericano() {
-        //chtob sdelat kfe nado nalit vodi, potom dobavit semena
-        var tankForHolyWater = 0
-        var tankForExpiredMilk = 0
-        var boxForEvilSeeds = 0
-        if tankForHolyWater == 0 {
-            print ("Hey, moron, you`d better add water or I`ll kick yo ass!")
-        } else {
-            print ("If you ever use this water again, I'm going to kill U!")
-        }
-        tankForHolyWater += 6
-        if boxForEvilSeeds == 0 {
-            print ("Box for seeds is empty, fool!")
-        } else {
-            print ("If you don`t want to die, stop adding seeds!")
-        }
-        boxForEvilSeeds += 6
-        
-        
+        makeShitCoffee(name: "americano", seedsNeed: 13, holyWaterNeed: 20, expiredMilkNeed: 7, cocaineNeed: 1)
     }
+    
+    func makingCappuccinoCocaina() {
+        makeShitCoffee(name: "cappuccino", seedsNeed: 18, holyWaterNeed: 34, expiredMilkNeed: 11, cocaineNeed: 2)
+    }
+    
+//    func makingHotLikeHellAmericano() {
+//        if holyWater >= 13 {
+//            print ("Hey, moron, you`d better add water or I`ll kick yo ass!")
+//        } else {
+//            print ("If you ever use this water again, I'm gonna kill U!")
+//        }
+//        if evilSeeds >= 13 {
+//            print ("Box for seeds is empty, fool!")
+//        } else {
+//            print ("Stop adding seeds if you want to live!")
+//        }
+//        print("Here is yo Americano, maded with love, bitch!")
+//    }
+    
+//    func makingCappuccinoCocaina() {
+//        if holyWater >= 13 {
+//        print ("Hey, moron, you`d better add water or I`ll kick yo ass!")
+//        } else {
+//            print ("If you ever use this water again, I'm gonna kill U!")
+//        }
+//        if evilSeeds >= 13 {
+//            print ("Box for seeds is empty, fool!")
+//        } else {
+//            print ("If you don`t want to die, stop adding seeds!")
+//        }
+//        if expiredMilk >= 6 {
+//            print ("If you want to save your soul, add milk!")
+//        } else {
+//            print ("Keep using this milk and you`ll die hard! ")
+//        }
+//        if cocaine >= 3 {
+//            print ("Need some Florida snow, dude!")
+//        } else {
+//            print ("I have speed, bro!")
+//        }
+//        print("Here is yo Cappuccino, cheer up mfk!")
+//    }
+    
 
 }
--- CoffeMachine/ViewController.swift
+++ CoffeMachine/ViewController.swift
@@ -10,25 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let deathMachine = DamnedCoffeMachine()
+    @IBOutlet weak var deathMachineMessagesLabel: UILabel!
+    
+    
+    
+    
+    @IBAction func buttonPressedForAmericano() {
+        deathMachine.makingHotLikeHellAmericano()
+    }
+    
+    @IBAction func buttonPressedForCappuccino() {
+        deathMachine.makingCappuccinoCocaina()
+    }
+    
+    
+    
+    @IBAction func buttonPressedAddSeeds() {
+        deathMachine.addEvilSeeds()
+        }
+    @IBAction func buttonPressedAddCocaine() {
+        deathMachine.addCocaine()
+    }
+    @IBAction func buttonPressedAddMilk() {
+        deathMachine.addExpiredMilk()
+    }
+    @IBAction func buttonPressedAddWater() {
+    deathMachine.addHolyWater()
+    }
+    
+    let deathMachine = DamnedCoffeMachine.init()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let deathMachine = DamnedCoffeMachine.init()
+        
+        deathMachine.addHolyWater()
+        deathMachine.addCocaine()
+        deathMachine.addEvilSeeds()
+        deathMachine.addExpiredMilk()
+        deathMachine.makingHotLikeHellAmericano()
+        deathMachine.makingCappuccinoCocaina()
+        showMessagesFromDethMachine()
+     
+        let stringToShow = deathMachine.makingHotLikeHellAmericano()
+        deathMachineMessagesLabel.text = 
+        
+        
+        
+        
         
         
     }
-    
-    
-    var hollyWater = DamnedCoffeMachine.init()
-    var expiredMilk = DamnedCoffeMachine.init()
-    var evilSeeds = DamnedCoffeMachine.init()
-    var tank = 0
-    
-    
-    
-    
+
+    private func showMessagesFromDethMachine() {
+        deathMachineMessagesLabel.text = "\(deathMachine.makingHotLikeHellAmericano())"
+    }
 }
 
